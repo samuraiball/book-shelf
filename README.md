8a155f96e41b43d08d9bb28c06659c6167ecdd60
hirooka <yuy.hiroihro@gmail.com>
2019-01-14 14:22:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# about this 
+this is the web api that register what you want to read and have read.
+
+
+# how to use
+TODO
---
+++ src/main/java/com/bookshelf/bookshelf/BookShelfApplication.java
@@ -0,0 +1,14 @@
+package com.bookshelf.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShelfApplication.class, args);
+	}
+
+}
+
