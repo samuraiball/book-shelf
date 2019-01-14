087b00ef3daedcea54048048d13ff8d88969f4bf
hirooka <yuy.hiroihro@gmail.com>
2019-01-14 18:43:58 +0900

    feature: modify circleCI setting

---
+++ src/main/java/com/bookshelf/dto/BookDto.java
@@ -0,0 +1,41 @@
+package com.bookshelf.dto;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+/**
+ * Book dto
+ */
+public class BookDto implements Serializable {
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    private String genre;
+
+
+    public BookDto(){
+    }
+
+    public BookDto(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
