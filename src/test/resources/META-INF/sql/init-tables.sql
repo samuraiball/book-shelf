ee59b919e4818f01ccb5a1b07aee8ce0d834713e
hirooka <yuy.hirohiro@gmail.com>
2019-01-19 15:44:47 +0900

    fix: modify a sql file

--- src/test/resources/META-INF/sql/init-tables.sql
+++ src/test/resources/META-INF/sql/init-tables.sql
@@ -1,3 +1,3 @@
 DELETE FROM books;
 
-INSERT INTO books(id, title, genre, is_active) VALUES(0L ,'WebAPI: The Good Part', 'IT', true);
+INSERT INTO books(id, title, genre, is_active) VALUES(0 ,'WebAPI: The Good Part', 'IT', true);
