aa17085e0aaa0f4ba47338ec25aab66839c48ac1
hirooka <yuy.hirohiro@gmail.com>
2019-06-13 00:27:06 +0900

    wip error handling controller advance

---
+++ src/main/java/com/bookshelf/model/dto/Builder/ErrorResourceBuilder.java
@@ -0,0 +1,31 @@
+package com.bookshelf.model.dto.Builder;
+
+import com.bookshelf.model.dto.ErrorResource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ErrorResourceBuilder {
+	private LocalDateTime timestamp;
+	private String message;
+	private List<ErrorResource.ErrorDto> errors;
+
+	public ErrorResourceBuilder withTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+		return this;
+	}
+
+	public ErrorResourceBuilder withMessage(String message) {
+		this.message = message;
+		return this;
+	}
+
+	public ErrorResourceBuilder withErrors(List<ErrorResource.ErrorDto> errors) {
+		this.errors = errors;
+		return this;
+	}
+
+	public ErrorResource createErrorResource() {
+		return new ErrorResource(timestamp, message, errors);
+	}
+}
